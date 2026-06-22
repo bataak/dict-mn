bfaf22172a4155b20c827c9746f9056e5a936aa8
bataak <bataak@gmail.com>
2026-06-22 23:14:36 +0800

    Add Cyrillic trimmer function to lunr

---
+++ assets/js/cyr-trimmer.js
@@ -0,0 +1,18 @@
+(function () {
+  function patch() {
+    if (!window.lunr || lunr.__cyr) return !!window.lunr;
+    lunr.trimmer = function (token) {
+      return token.update(function (s) {
+        return s.replace(/^[^\p{L}\p{N}_]+/u, '').replace(/[^\p{L}\p{N}_]+$/u, '');
+      });
+    };
+    lunr.Pipeline.registerFunction(lunr.trimmer, 'trimmer');
+    lunr.__cyr = true;
+    return true;
+  }
+  if (!patch()) {
+    var n = 0, id = setInterval(function () {
+      if (patch() || ++n > 200) clearInterval(id);
+    }, 10);
+  }
+})();
